0b4e0be83eb7adff62b77e871f4359d6c440c9b2
mnakamoto0305 <m.nakamoto0305@gmail.com>
2021-03-01 16:36:38 +0900

    first commit

---
+++ src/main/java/com/sample/springboot/DateCalculationAppApplication.java
@@ -0,0 +1,13 @@
+package com.sample.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateCalculationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateCalculationAppApplication.class, args);
+	}
+
+}
