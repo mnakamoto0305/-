6bd71df5a51049c68c349da801d536e7a5d6a405
mnakamoto0305 <m.nakamoto0305@gmail.com>
2021-03-10 15:35:59 +0900

    修正：ログインエラー時のメッセージ修正

--- src/main/java/com/sample/springboot/Service/DatabaseUserDetailsService.java
+++ src/main/java/com/sample/springboot/Service/DatabaseUserDetailsService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.sample.springboot.mybatis.UserMapper;
 
@@ -17,6 +18,16 @@ public class DatabaseUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		// TODO 自動生成されたメソッド・スタブ
+		if (StringUtils.isEmpty(username)) {
+		    throw new UsernameNotFoundException("username is empty");
+		  }
+
+		UserDetails user = userMapper.identifyUser(username);
+
+		if (user == null) {
+		    throw new UsernameNotFoundException("Not found username : " + username);
+		  }
+
 		return userMapper.identifyUser(username);
 	}
 
